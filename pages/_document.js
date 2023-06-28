2ab17085bf33ddc14b1f4b7f6f965e917e6c998b
DheerajB <Prajwal.Rathod@hhstechgroup.com>
2023-06-28 17:39:51 +0530

    push

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ class MyDocument extends Document {
                 __html: 'window.liveSettings={api_key:"e69fca72d6b743f4a850fa9a9a51846f"}',
               }}
             ></script>
+            <script type="text/javascript" src="//cdn.transifex.com/live.js"></script>
           </Head>
           <body>
             <Main />
